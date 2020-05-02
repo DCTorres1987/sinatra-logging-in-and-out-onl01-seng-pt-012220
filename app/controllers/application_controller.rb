a6afc843f852b5a4cf37d093f78c705e443430a4
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-05-02 10:59:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
+     if !!@user == false 
+       erb :error 
+     else 
+       redirect to ''
      binding.pry
   end
 
