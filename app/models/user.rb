4c02f6c65b7d1306114bf466897b4612a01caa8b
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-05-02 10:23:06 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User  < ActiveRecord::Base
 
+attr_accessor :username, :password, :balance 
+
+@@all = []
+
+def initialize(username, password, balance)
+  @username = username
+  @password = password 
+  @balance = balance 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def self.destroy_all 
+  @@all.clear 
+end 
   
 end
 
